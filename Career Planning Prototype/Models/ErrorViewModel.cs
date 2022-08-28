556672a858f6762a8318847bed49a63339304f8e
Blakedasnake727 <barrblake555@gmail.com>
2022-08-28 19:35:37 -0400

    Add project files.

---
+++ Career Planning Prototype/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Career_Planning_Prototype.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
